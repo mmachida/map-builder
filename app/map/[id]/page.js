9346e63878a6e6fac310783df6b982a583e10754
mmachida <mmachida@users.noreply.github.com>
2026-04-25 04:02:55 -0300

    Add files via upload

--- app/map/[id]/page.js
+++ app/map/[id]/page.js
@@ -84,7 +84,7 @@ export default function PublicMapPage() {
           initialScale={1}
           minScale={0.5}
           maxScale={5}
-          wheel={{ step: 0.05 }}
+          wheel={{ step: 0.01 }}
           doubleClick={{ disabled: true }}
         >
           {({ zoomIn, zoomOut, resetTransform }) => (
